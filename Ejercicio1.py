3e96aa2b613b59587871dd4ea548b2264e4672c3
Thiago Mowszet <thiagomowszet@gmail.com>
2022-04-19 14:39:44 -0300

    "Dado un numero devolver su tabla de multiplicar completa"

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,6 +1,6 @@
 # Ejercicio Logica 1
 
-""" 
+"""
 
 Dado un numero devolver su tabla de multiplicar completa
 
@@ -10,19 +10,11 @@ como hacerlo:
 - bucle del 1 al 10
 - concatenar la variable string con multiplicación y resultado
 
-
 """
 
+def tablasMultiplicar(x):
+    for i in range(11):
+        print(x, "por", i, "es igual a:", x*i)
 
-# Decidi hacerlo sin una funcion, tambien que sea solamente las tablas del 1 al 10. Si se desea lo contrario, eliminar las lineas 23 y 24.
-
-n = int(input("Elija un numero para saber su tabla de multiplicar: ")) # El usuario ingresa un numero.
-
-if n < 0:
-    print("Debe de ingresar un numero positivo entre el 1 y el 10") # Valida si el input del usuario es menor a 0 o negativo.
-elif n > 10:
-    print("Debe de elegir un numero entre el 1 y el 10") # Valida si el input del usuario es mayor a 10.
-
-
-for i in range(1,11): 
-    print(n, "por", i, "es igual a:", n*i)
+x = int(input("Seleccione un numero: "))
+llamar = tablasMultiplicar(x)
