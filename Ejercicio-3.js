e26c77b00921d8f1be11c2204c9313330e66e1a1
Thiago <thiagomowszet@gmail.com>
2022-08-18 01:21:46 -0300

    Tablas Multiplicar

---
+++ Ejercicio-3.js
@@ -0,0 +1,34 @@
+/*
+
+Dado un numero devolver su tabla de multiplicar completa
+como hacerlo:
+- función con parametro numero
+- variable de texto encabezado
+- bucle del 1 al 10
+- concatenar la variable string con multiplicación y resultado
+
+*/
+
+
+const tablaMultiplicar = (a) => { 
+    for ( let i = 0 ; i < 11 ; i++ ){
+        console.log(`${a} por ${i} es igual a ${a * i}`)
+    }
+}
+
+tablaMultiplicar()
+
+
+/* 
+
+Funcion normal 
+
+function tablasMultiplicar(a){
+    for ( let i = 0 ; i < 11 ; i++ ){
+        console.log(`${a} por ${i} es igual a ${a * i}`)
+    }
+}
+
+tablasMultiplicar()
+
+*/
