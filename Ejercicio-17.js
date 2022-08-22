9c05ace7e039cc706424ac817606d8ddf3745a69
Thiago <thiagomowszet@gmail.com>
2022-08-22 14:53:12 -0300

    Sumar valor minMax del array

---
+++ Ejercicio-17.js
@@ -0,0 +1,36 @@
+
+
+let lista = [1, 2, 3, 4, 56, 7, 8, 10]
+let suma
+let a
+
+
+const minMax = () => {
+        lista.sort((a, b) => a - b)
+        suma = lista[0] + lista[lista.length - 1]
+
+        console.log(lista)
+        console.log(suma)
+}
+
+minMax()
+
+
+/*
+
+Funcion Normal
+
+function minMax() {
+
+        lista.sort((a,b) => a - b)
+        suma = lista[0] + lista[lista.length - 1]
+
+        console.log(lista)
+        console.log(suma)
+
+
+}
+
+minMax()
+
+*/
