3c78a5ea893bc42a5eecdc968cbcde8dcdf0fdb5
Thiago <thiagomowszet@gmail.com>
2022-08-22 14:45:59 -0300

    Ordenar Lista

--- Ejercicio-11.js
+++ Ejercicio-11.js
@@ -4,8 +4,8 @@ Ordenar la lista de números de menor a mayor.
 
 */
 
-let lista = [1,6,5,18,24,9,0,80,11]
+let lista = [1, 6, 5, 18, 24, 9, 0, 80, 11]
 
-let a = lista.sort((a, b) => a - b) 
+let a = lista.sort((a, b) => a - b)
 
 console.log(a)
