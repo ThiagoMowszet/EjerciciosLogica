06a23b2c5ef0f0acae37567cc0dea21df0ecfe28
Thiago <thiagomowszet@gmail.com>
2022-08-22 14:45:36 -0300

    Leer Lista de positivos y negativos

--- Ejercicio-10.js
+++ Ejercicio-10.js
@@ -6,18 +6,18 @@ Escribir un programa que lea una lista de números y determine cuantos son posit
 
 const listaNumeros = [-1, 1, -2, 2, 3, -3, 4, -4, -5, -6, 5, 6]
 
-let positivo = 0 
-let negativo = 0 
+let positivo = 0
+let negativo = 0
 
 
 for (let i = 0; i < listaNumeros.length; i++) {
-    
-    if (listaNumeros[i] > 0){
+
+    if (listaNumeros[i] > 0) {
         positivo++
     } else {
         negativo++
     }
-    
+
 }
 
 console.log(`Hay ${positivo} numeros positivos`)
